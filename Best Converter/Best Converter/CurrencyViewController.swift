34fce8c819cf435f41b938ebee1e3a8136049c10
Asma <Asma.Abdullah.has.s@tuwaiq.edu.sa>
2021-11-08 02:08:10 +0300

    Writing code to convert units and convert currencies

--- Best Converter/Best Converter/CurrencyViewController.swift
+++ Best Converter/Best Converter/CurrencyViewController.swift
@@ -6,24 +6,75 @@
 //
 
 import UIKit
+import Foundation
 
-class CurrencyViewController: UIViewController {
+enum ConversionCurrency {
+    case SAR
+    case USD
+}
+
+class CurrencyViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate {
+
+    var conversionCurrency = ConversionCurrency.SAR
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        textFieldCurrency.delegate = self
+        
+        //MARK: - Tap Gesture Recognizer, dismiss the keyboard when the user taps on the screen
+                let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing))
+                view.addGestureRecognizer(tap)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var textFieldCurrency: UITextField!
+    @IBOutlet weak var segmentCurrency: UISegmentedControl!
+    @IBAction func egmentCurrencyAction(_ sender: UISegmentedControl) {
+        
+        textFieldCurrency.text = ""
+        switch segmentCurrency.selectedSegmentIndex {
+        case 0:
+            conversionCurrency = .SAR
+            currencyLabel.text = "0.0 USD"
+        case 1:
+            conversionCurrency = .USD
+            currencyLabel.text = "0.0 SAR"
+        default:
+            conversionCurrency = .SAR
+            currencyLabel.text = "0.0 USD"
+        }
     }
-    */
-
+    
+    func convertToSar( usd: Double) -> Double {
+        return usd / 3.75
+    }
+    func convertToUsd( sar: Double) -> Double {
+        return sar * 3.75
+    }
+    
+    
+    @IBAction func textFieldCurrencyAction(_ sender: UITextField) {
+        
+        let amountCurrency = Double(textFieldCurrency.text ?? "0.0") ?? 0.0
+        
+        switch conversionCurrency {
+        case .SAR:
+            let convertedCurrency = convertToSar(usd: amountCurrency)
+            currencyLabel.text = "\(String(format: "%.4f", convertedCurrency)) USD"
+            
+        case .USD:
+            let convertedCurrency = convertToUsd(sar: amountCurrency)
+            currencyLabel.text = "\(String(format: "%.4f", convertedCurrency)) SAR"
+        }
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+         
+        if let lessCount = textFieldCurrency.text?.count, lessCount < 4 {
+            return true
+        } else { return false }
+    }
+    
 }
--- Best Converter/Best Converter/UnitViewController.swift
+++ Best Converter/Best Converter/UnitViewController.swift
@@ -6,18 +6,77 @@
 //
 
 import UIKit
+import Foundation
 
-class UnitViewController: UIViewController {
+enum ConversionUnit {
+    case m
+    case km
+}
 
+class UnitViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate {
+    
+    var conversionUnit = ConversionUnit.m
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        inputTextField.delegate = self
+        
+        //MARK: - Tap Gesture Recognizer, dismiss the keyboard when the user taps on the screen
+                let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing))
+                view.addGestureRecognizer(tap)
 
-        // Do any additional setup after loading the view.
     }
     
-
+    @IBOutlet weak var conversionLabel: UILabel!
+    @IBOutlet weak var inputTextField: UITextField!
+    @IBOutlet weak var inputSegment: UISegmentedControl!
     
+    @IBAction func inputSegmentAction(_ sender: UISegmentedControl) {
+        inputTextField.text = ""
+        
+        switch inputSegment.selectedSegmentIndex {
+        case 0:
+            conversionUnit = .m
+            conversionLabel.text = "0.0 km"
+        case 1:
+            conversionUnit = .km
+            conversionLabel.text = "0.0 m"
+        default:
+            conversionUnit = .m
+            conversionLabel.text = "0.0 km"
+        }
+        
+    }
     
+    func convertToM( kilometer: Double) -> Double {
+        return kilometer * 1000
+    }
+    func convertToKm( meter: Double) -> Double {
+        return meter * 0.001
     }
     
     
+    @IBAction func inputTextFieldAction(_ sender: UITextField) {
+        
+        let amountUnit = Double(inputTextField.text ?? "0.0") ?? 0.0
+        
+        switch conversionUnit {
+        case .m:
+            let convertedValue = convertToM(kilometer: amountUnit)
+            conversionLabel.text = "\(convertedValue) km"
+        case .km:
+            let convertedValue = convertToKm(meter: amountUnit)
+            conversionLabel.text = "\(convertedValue) m"
+        }
+    }
+    
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+         
+        if let lessCount = inputTextField.text?.count, lessCount < 4 {
+            return true
+        } else { return false }
+    }
+    
+}
