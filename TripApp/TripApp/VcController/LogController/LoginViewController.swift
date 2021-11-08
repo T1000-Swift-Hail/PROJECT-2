4f71a2c14e7d794a30eb85de30deb94740ce9c4c
Hind Alharbi <Hind.Alharbi.has.s@tuwaiq.edu.sa>
2021-11-08 19:03:57 +0300

    Version_1, done with login and lsign out

---
+++ TripApp/TripApp/VcController/LogController/LoginViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LoginViewController.swift
+//  TripApp
+//
+//  Created by Hind Alharbi on 11/8/21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var FullNameText: UITextField!
+    @IBOutlet weak var PasswordText: UITextField!
+    @IBOutlet weak var onClickSucessLogIn: UIView!
+    @IBOutlet weak var LoginBton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnSuccssLogIn(_ sender: Any) {
+        
+    
+    }
+    
+}
