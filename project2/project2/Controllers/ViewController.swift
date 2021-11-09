0bbf98e39d764f783a44d794cd5ebe4486310aff
Mastorah <mas@Mastorahs-MacBook-Air.local>
2021-11-09 09:05:47 +0300

    Done worked in product interface

--- project2/project2/Controllers/ViewController.swift
+++ project2/project2/Controllers/ViewController.swift
@@ -8,39 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource,UITextFieldDelegate {
-   
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 4
     }
     
-   // func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-       // let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-       // return cell
-    }
+    // func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    // let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+    // return cell
     
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       print("viewDidLoad") // Do any additional setup after loading the view.
+        print("viewDidLoad") // Do any additional setup after loading the view.
     }
     override func viewWillAppear(_ animated: Bool) {
         print("viewWillAppear")
     }
     @IBOutlet weak var LblTitl: UILabel!
     @IBOutlet weak var txtusername: UITextField!
-   
+    
     @IBOutlet weak var txtpassward: UITextField!
     override func viewDidAppear(_ animated: Bool) {
         print("viewDidAppear")
     }
     
     @IBAction func btnlogin(_ sender: Any) {
-        LblTitl.text = Logged in
+        LblTitl.text = "Logged in"
     }
+//
+//    func textFieldShouldRuturn(_ textField: UITextField) -> Bool {
+//        if textField == txtusername {
+//            txtusername.becomeFirstResponder()
+//        }else{
+//            return true
+//        }
+//
+//    }
+    
 }
-func textFieldShouldRuturn(- textField: UITextField) -> Bool {
-    if textField == txtusername {
-        txtusername.becomeFirstResponder()
-    }else{
-        return true
-    }
