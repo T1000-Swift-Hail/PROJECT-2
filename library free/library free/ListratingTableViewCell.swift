c305e7dc6ff7cdb7190711c80a628aa5635323e1
موضي الحربي <modhialhrbi@MacBook-Air-mwdy.local>
2021-11-09 15:15:16 +0300

    completed main of reating

---
+++ library free/library free/ListratingTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ListratingTableViewCell.swift
+//  library free
+//
+//  Created by موضي الحربي on 04/04/1443 AH.
+//
+
+import UIKit
+
+class ListratingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
