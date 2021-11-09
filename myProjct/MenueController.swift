b14e602206077c6b347549db97ff74f0e204742f
Huda N S <hnsa5@MacBook-Air-Huda.local>
2021-11-09 16:33:12 +0300

    Translate the entire application into Arabic

--- myProjct/MenueController.swift
+++ myProjct/MenueController.swift
@@ -19,27 +19,18 @@ class MenueController: UIViewController , UITableViewDelegate , UITableViewDataS
         
         //Adding products to the TableView using the Stract
         arrayProduct.append(Pruducts.init(name: NSLocalizedString("Urmaki sushi", comment: ""), price: 73.00 , discrip: NSLocalizedString("detailsProduct1", comment: ""), photo: UIImage(named: "Soshi1")!))
-        
         arrayProduct.append(Pruducts.init(name: NSLocalizedString("Oshi Sushi", comment: ""), price: 80.00 , discrip: NSLocalizedString("detailsProduct2", comment: ""), photo: UIImage(named: "img13")!))
-        
-        
         arrayProduct.append(Pruducts.init(name: NSLocalizedString("Marssai Sushi Cocktail", comment: ""), price: 98.00 , discrip: NSLocalizedString("detailsProduct3", comment: ""), photo: UIImage(named: "img8")!))
-        
         arrayProduct.append(Pruducts.init(name: NSLocalizedString("Hoso Maki Roll", comment: "") , price: 88.00 , discrip: NSLocalizedString("detailsProduct4", comment: ""), photo: UIImage(named: "img23")!))
-        
-        
         arrayProduct.append(Pruducts.init(name: NSLocalizedString("Crab salad", comment: ""), price: 44.00 , discrip: NSLocalizedString("detailsProduct5", comment: ""), photo: UIImage(named: "Soshi1")!))
-        
-        
-        arrayProduct.append(Pruducts.init(name: NSLocalizedString("Shrimp", comment:"" ), price: 44.00 , discrip: "Crispy shrimp with cocktail sauce", photo: UIImage(named: "img4")!))
-        
-        arrayProduct.append(Pruducts.init(name: "Chicken tikka masala", price: 77.00 , discrip: "Djmsa marsai dish with indian flavor", photo: UIImage(named: "img12")!))
-        arrayProduct.append(Pruducts.init(name: "Fettuccine", price: 45.00 , discrip: "Fettuccine with italian flavor", photo: UIImage(named: "img2")!))
-        arrayProduct.append(Pruducts.init(name: "Risotto Seafood", price: 55.00 , discrip: "Tasting pleasure in Marssai", photo: UIImage(named: "img9")!))
-        arrayProduct.append(Pruducts.init(name: "Beef Lasagna", price: 65.00 , discrip: "Lasagna with fresh meat", photo: UIImage(named: "img3")!))
-        arrayProduct.append(Pruducts.init(name: "Hot kibok", price: 33.00 , discrip: "Chili flavored kibok", photo: UIImage(named: "img10")!))
-        arrayProduct.append(Pruducts.init(name: "Appetizer mix", price: 32.00 , discrip: "Marssai special appetizer", photo: UIImage(named: "img14")!))
-        arrayProduct.append(Pruducts.init(name: "Tabbouleh", price: 38.00 , discrip: "Tabbouleh pineapple", photo: UIImage(named: "img7")!))
+        arrayProduct.append(Pruducts.init(name: NSLocalizedString("Shrimp", comment:"" ), price: 44.00 , discrip:NSLocalizedString("detailsProduct6", comment: ""), photo: UIImage(named: "img4")!))
+        arrayProduct.append(Pruducts.init(name: NSLocalizedString("Chicken tikka masala", comment:"") , price: 77.00 , discrip: NSLocalizedString("detailsProduct7", comment:"" ), photo: UIImage(named: "img12")!))
+        arrayProduct.append(Pruducts.init(name: NSLocalizedString("Fettuccine ", comment: ""), price: 45.00 , discrip: NSLocalizedString("detailsProduct8", comment:""), photo: UIImage(named: "img2")!))
+        arrayProduct.append(Pruducts.init(name: NSLocalizedString("Risotto Seafood", comment: "") , price: 55.00 , discrip: NSLocalizedString("detailsProduct9", comment: ""), photo: UIImage(named: "img9")!))
+        arrayProduct.append(Pruducts.init(name:NSLocalizedString("Beef Lasagna", comment:""), price: 65.00 , discrip: NSLocalizedString("detailsProduct10", comment: ""), photo: UIImage(named: "img3")!))
+        arrayProduct.append(Pruducts.init(name: "Hot kibok", price: 33.00 , discrip: NSLocalizedString("detailsProduct11", comment: "" ), photo: UIImage(named: "img10")!))
+        arrayProduct.append(Pruducts.init(name:NSLocalizedString("Appetizer mix", comment: "") , price: 32.00 , discrip:NSLocalizedString("detailsProduct12", comment: ""), photo: UIImage(named: "img14")!))
+        arrayProduct.append(Pruducts.init(name: NSLocalizedString("Tabbouleh", comment: ""), price: 38.00 , discrip:NSLocalizedString("detailsProduct13", comment: "" ), photo: UIImage(named: "img7")!))
     }
     //function to display products in Table View
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
